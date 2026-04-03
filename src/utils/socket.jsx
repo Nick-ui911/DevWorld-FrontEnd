3f672e1a242e4dff71211797f32bee19493b5de4
Nick-ui911 <nbaghel392@gmail.com>
2026-04-04 00:03:56 +0530

    updated socket config

--- src/utils/socket.jsx
+++ src/utils/socket.jsx
@@ -1,15 +1,9 @@
 import { io } from "socket.io-client";
 import { BASE_URL } from "./constants";
+
 export const createSocketConnection = () => {
-  if (location.hostname === "localhost") {
-    // 🖥️ If running locally, connect using BASE_URL (e.g., http://localhost:5000)
-    return io(BASE_URL);
-  } else {
-    //      In production, io("/") means
-    // 👉 connect to the same domain where the frontend is loaded from
-    // Example:
-    // Frontend: https://book-your-event.vercel.app
-    // Socket connects to: https://book-your-event.vercel.app
-    return io("/", { path: "/api/socket.io" });
-  }
+  return io(BASE_URL, {
+    withCredentials: true,
+    transports: ["websocket"], // optional but good
+  });
 };
